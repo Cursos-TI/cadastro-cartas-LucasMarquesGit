5c8cc4d9715ba6956fa4803d06544735620ccd30
LucasMarquesGit <lucasmarquesdasilva312@gmail.com>
2025-10-02 15:38:03 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -69,10 +69,10 @@ int main() {
 
     // Saída dos dados da Carta 1
     printf("\n===== Carta 1 =====\n");
-    printf("Estado: %c\n", estado1);
+    printf("amazonas %c\n", estado1);
     printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeCidade1);
-    printf("População: %d\n", populacao1);
+    printf("Mata %s\n", nomeCidade1);
+    printf("1.000.000.000 %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
